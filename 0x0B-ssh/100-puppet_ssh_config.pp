a077c51786fb949900a98d317e8fa3b5515462fa
Fenet-damena <fenetdamena74@gmail.com>
2024-08-23 10:48:13 -0700

    dd

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++
@@ -1,5 +0,0 @@
-# Create an SSH configuration file
-file { '/etc/ssh/ssh_config':
-  ensure  => file,
-  content => "# SSH configuration file\nIdentityFile ~/.ssh/holberton\nPasswordAuthentication no",
-}
